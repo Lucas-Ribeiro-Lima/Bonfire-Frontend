c3baf6894f26f021b972a863d66359ca1393432f
Lucaslive974 <lucasribeirolima974@gmail.com>
2024-04-01 22:50:59 -0300

    feat: add date default value, convert date columns to legible and add filters

--- src/components/infractions/infractionLayout.tsx
+++ src/components/infractions/infractionLayout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { FetchInfractionsFirstInstance } from '@/hooks/fetchData'
+import { DEFAULTDATA } from '@/services/utils'
 import {
   Pagination,
   Spinner,
@@ -12,34 +13,64 @@ import {
   TableRow,
   getKeyValue,
 } from '@nextui-org/react'
-import { useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
+import { z } from 'zod'
 
-export type autosData = {
-  autos: {
-    NUM_NOTF: string
-    TIP_PENL: string
-    NUM_AI: string
-    NOM_CONC: string
-    COD_LINH: number
-    NOM_LINH: string
-    NUM_VEIC: number
-    IDN_PLAC_VEIC: string
-    DAT_OCOR_INFR: Date
-    DES_LOCA: string
-    COD_IRRG_FISC: number
-    ARTIGO: string
-    DESC_OBSE: string
-    NUM_MATR_FISC: number
-    QTE_PONT: number
-    DAT_EMIS_NOTF: Date
-    DAT_LIMIT_RECU: Date
-    VAL_INFR: number
-    DAT_CANC: Date
-  }[]
-  counter: {
-    TOTAL: number
-  }[]
-}
+const autosSchema = z.object({
+  autos: z
+    .object({
+      NUM_NOTF: z.string(),
+      TIP_PENL: z.string(),
+      NUM_AI: z.string(),
+      NOM_CONC: z.string(),
+      COD_LINH: z.number(),
+      NOM_LINH: z.string(),
+      NUM_VEIC: z.number(),
+      IDN_PLAC_VEIC: z.string(),
+      DAT_OCOR_INFR: z.number(),
+      DAT_OCOR_INFR_STR: z.string(),
+      DES_LOCA: z.string(),
+      COD_IRRG_FISC: z.string(),
+      ARTIGO: z.string(),
+      DESC_OBSE: z.string(),
+      NUM_MATR_FISC: z.number(),
+      QTE_PONT: z.number(),
+      DAT_EMIS_NOTF: z.number(),
+      DAT_EMIS_NOTF_STR: z.string(),
+      DAT_LIMIT_RECU: z.date(),
+      VAL_INFR: z.number(),
+      DAT_CANC: z.date(),
+    })
+    .array(),
+})
+
+export type autosData = z.infer<typeof autosSchema>
+// export type autosData = {
+//   autos: {
+//     NUM_NOTF: string
+//     TIP_PENL: string
+//     NUM_AI: string
+//     NOM_CONC: string
+//     COD_LINH: number
+//     NOM_LINH: string
+//     NUM_VEIC: number
+//     IDN_PLAC_VEIC: string
+//     DAT_OCOR_INFR: Date
+//     DES_LOCA: string
+//     COD_IRRG_FISC: number
+//     ARTIGO: string
+//     DESC_OBSE: string
+//     NUM_MATR_FISC: number
+//     QTE_PONT: number
+//     DAT_EMIS_NOTF: Date
+//     DAT_LIMIT_RECU: Date
+//     VAL_INFR: number
+//     DAT_CANC: Date
+//   }[]
+//   counter: {
+//     TOTAL: number
+//   }[]
+// }
 
 const columns = [
   {
@@ -75,7 +106,7 @@ const columns = [
   //     label: "Placa",
   // },
   {
-    key: 'DATA_OCOR_INFR',
+    key: 'DAT_OCOR_INFR_STR',
     label: 'Data Ocorrência',
   },
   {
@@ -103,7 +134,7 @@ const columns = [
   //     label: "Pontuação",
   // },
   {
-    key: 'DAT_EMIS_NOTF',
+    key: 'DAT_EMIS_NOTF_STR',
     label: 'Data Emissão Notificação',
   },
   // {
@@ -122,28 +153,78 @@ const columns = [
 
 export default function InfractionLayout() {
   // Data handling
-  const [date, setDate] = useState<string>('2024-01-01')
-  const handleDateChange = (event: any) => {
-    const newDate = event.target.value
-    setDate(newDate)
-  }
+  const [date, setDate] = useState<string>(DEFAULTDATA)
+  const [page, setPage] = useState(1)
+  const [veicValueFilter, setVeicValueFilter] = useState('')
+  const [linhaValueFilter, setLinhaValueFilter] = useState('')
+  const [autoValueFilter, setAutoValueFilter] = useState('')
+  const [rowsPerPage] = useState<number>(15)
+
+  const hasVeicFilter = Boolean(veicValueFilter)
+  const hasLinhaFilter = Boolean(linhaValueFilter)
+  const hasAutoFilter = Boolean(autoValueFilter)
+
   // Data fetch
   const { data, error } = FetchInfractionsFirstInstance<autosData>(
     `/autoInfracao/primeiraInstancia/${date}`,
   )
 
-  // Pagination
-  const [page, setPage] = useState(1)
-  const [rowsPerPage] = useState<number>(15)
+  data.autos.forEach((auto) => {
+    auto.DAT_EMIS_NOTF_STR = new Date(auto.DAT_EMIS_NOTF).toLocaleDateString()
+    auto.DAT_OCOR_INFR_STR = new Date(auto.DAT_OCOR_INFR).toLocaleDateString()
+  })
 
-  const pages = Math.ceil(data?.autos.length / rowsPerPage)
+  const filteredItems = useMemo(() => {
+    let filteredData = [...(data.autos ?? [])]
+
+    if (hasVeicFilter) {
+      filteredData = filteredData.filter((auto) =>
+        auto.NUM_VEIC?.toString().includes(veicValueFilter),
+      )
+    }
+
+    if (hasAutoFilter) {
+      filteredData = filteredData.filter((auto) =>
+        auto.NUM_AI?.includes(autoValueFilter),
+      )
+    }
+
+    if (hasLinhaFilter) {
+      filteredData = filteredData.filter((auto) =>
+        auto.COD_LINH.toString().includes(linhaValueFilter),
+      )
+    }
+
+    return filteredData
+  }, [data, veicValueFilter, linhaValueFilter, autoValueFilter])
+
+  // Pagination
+  const pages = useMemo(() => {
+    return Math.ceil((filteredItems || []).length / rowsPerPage)
+  }, [rowsPerPage, filteredItems])
 
   const items = useMemo(() => {
     const start = (page - 1) * rowsPerPage
     const end = start + rowsPerPage
 
-    return data?.autos.slice(start, end)
-  }, [page, data])
+    return filteredItems.slice(start, end)
+  }, [page, data, filteredItems])
+
+  function handleDateChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setDate(event.target.value)
+  }
+
+  function handleVeicChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setVeicValueFilter(event.target.value)
+  }
+
+  function handleAiChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setAutoValueFilter(event.target.value)
+  }
+
+  function handleLinhaChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setLinhaValueFilter(event.target.value)
+  }
 
   // Early returns
   if (error) return <div>{error.message}</div>
@@ -159,10 +240,38 @@ export default function InfractionLayout() {
             <>Data</>
             <input
               onChange={handleDateChange}
+              defaultValue={DEFAULTDATA}
               type="date"
               className="w-36 rounded-lg bg-white/60 pl-1 text-black"
             ></input>
             {/* <input type="date" className="w-36 pl-1 rounded-lg bg-white/60 text-black"></input> */}
+          </label>
+          <label className="flex gap-2 rounded-lg font-semibold text-white">
+            <>N° AI</>
+            <input
+              onChange={handleAiChange}
+              defaultValue=""
+              type="text"
+              className="w-36 rounded-lg bg-white/60 pl-1 text-black"
+            ></input>
+          </label>
+          <label className="flex gap-2 rounded-lg font-semibold text-white">
+            <>Veiculo</>
+            <input
+              onChange={handleVeicChange}
+              defaultValue=""
+              type="text"
+              className="w-36 rounded-lg bg-white/60 pl-1 text-black"
+            ></input>
+          </label>
+          <label className="flex gap-2 rounded-lg font-semibold text-white">
+            <>Linha</>
+            <input
+              onChange={handleLinhaChange}
+              defaultValue=""
+              type="text"
+              className="w-36 rounded-lg bg-white/60 pl-1 text-black"
+            ></input>
           </label>
         </div>
       </div>
---
+++ src/services/utils.ts
@@ -0,0 +1,3 @@
+export const DEFAULTDATA = new Date().toISOString().split('T')[0]
+// export const DEFAULTYEAR = new Date().getFullYear()
+// export const DEFAULTMONTH = convertMonth[new Date().getMonth()]
