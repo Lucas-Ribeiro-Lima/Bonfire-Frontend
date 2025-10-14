4ec979314c227cfe57b8a15bfa22a170797e21a1
lucas.lima <lucasribeirolima974@gmail.com>
2025-10-14 16:14:02 -0300

    Refactor: Remove redundancy in apiClient initialization.

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,14 +1,8 @@
 import axios from 'axios'
 
-function ApiClient() {
-  const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}`,
-    headers: {
-      'X-Custom-Header': 'foobar',
-    },
-  })
-
-  return api
-}
-
-export const api = ApiClient()
+export const api = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}`,
+  headers: {
+    'X-Custom-Header': 'foobar',
+  },
+})
