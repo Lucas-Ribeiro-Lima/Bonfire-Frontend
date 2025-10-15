7e308d0c7062c7032b10bfea8d9b60d13f4c1fbd
lucas.lima <lucasribeirolima974@gmail.com>
2025-10-15 14:57:03 -0300

    Fix: Consult keycloak if the session is valid every authentication flow.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,28 @@
 import { withAuth } from 'next-auth/middleware'
+import { env } from '@/services/env'
 
 export default withAuth({
+  secret: env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/login',
     signOut: '/login',
+  },
+  callbacks: {
+    async authorized({ token }) {
+      if (!token) return false
+
+      const response = await fetch(
+        `${env.KEYCLOAK_ISSUER}/protocol/openid-connect/userinfo`,
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Bearer ${token['accessToken']}`,
+          },
+        },
+      )
+
+      return response.ok
+    },
   },
 })
 
@@ -12,6 +31,6 @@ export const config = {
     '/infractions/:path*',
     '/registers/:path*',
     '/import/:path*',
-    '/recurses/:path',
+    '/recurses/:path*',
   ],
 }
